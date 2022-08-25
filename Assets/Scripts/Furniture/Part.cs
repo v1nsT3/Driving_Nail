1486ca46f9f814b021d2e74282f2b0b5123ab335
v1nsT3 <105610480+v1nsT3@users.noreply.github.com>
2022-08-25 18:03:18 +0500

    Increase speed

--- Assets/Scripts/Furniture/Part.cs
+++ Assets/Scripts/Furniture/Part.cs
@@ -1,15 +1,22 @@
 using UnityEngine;
 using DG.Tweening;
+using System.Collections;
 
 public class Part : MonoBehaviour
 {
-    private float _durationMove = 1.5f;
+    private float _durationMove = 0.7f;
     private Tween _tween;
 
     public bool IsMove => _tween.IsActive();
 
     private void OnEnable()
     {
+        StartCoroutine(MoveDelya());
+    }
+
+    private IEnumerator MoveDelya()
+    {
+        yield return new WaitForSeconds(1f);
         _tween = transform.DOLocalMove(Vector3.zero, _durationMove);
     }
 }
